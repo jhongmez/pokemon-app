647332c2a99d932c8c1e4c8054fa906d7b3b26fd
jhongmez <gmezjhon@gmail.com>
2021-08-13 14:23:44 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   { 
     path: 'home', 
     loadChildren: () => import('./components/pokemons/pokemon-list/pokemon-list.module')
